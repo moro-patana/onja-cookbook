b10d09dfa5f99f0ce725da36c00899df7e0c95b3
Manjaka Randrianirina <manjaka.ran@onja.org>
2020-07-25 10:37:38 +0300

    grab elements

---
+++ script.js
@@ -0,0 +1,9 @@
+// Grab elements
+const bookList = document.querySelector(`.book`);
+const formInput = document.querySelector(`.post-form`);
+const submitBtn = document.querySelector(`.btn-primary`);
+const addIngredientBtn = document.querySelector(`.add-ingredient`);
+const addStepsBtn = document.querySelector(`.add-steps`);
+
+
+// Listen to the submit button
